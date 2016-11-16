b7284ecfd3f42d367467d7f1481091353d47219b
Aaryadev <devmgbhat@gmail.com>
2016-11-16 18:39:57 +0530

    Basic
    INIT

--- server.js
+++ server.js
@@ -1,1 +1,17 @@
 
+var static = require('node-static');
+var file = new static.Server('./UI');
+
+//vars
+var userCount=0;
+var chanels=[];
+
+//Define
+var server = require('http').createServer(function(request, response) {
+    request.addListener('end', function() {
+        file.serve(request, response);
+    }).resume();
+});
+var io =require('socket.io')(server);
+server.listen(process.env.PORT || 9999);
+console.log("Server Started");
